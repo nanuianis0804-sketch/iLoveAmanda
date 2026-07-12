7ff7dffbb983519ed906169d02e049f429468224
nanuianis0804-sketch <nanuianis0804@gmail.com>
2026-07-13 02:39:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function moveRandomEl(elm) {
   elm.style.position = "absolute";
-  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
   elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
 
 
@@ -11,3 +11,4 @@ const moveRandom = document.querySelector("#move-random");
 
 moveRandom.addEventListener("mouseenter", function (e) {
   moveRandomEl(e.target);
+}};
