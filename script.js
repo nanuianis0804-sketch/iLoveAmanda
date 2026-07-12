b5da74137c00a37cd2f61b114fa0e735919a5c24
nanuianis0804-sketch <nanuianis0804@gmail.com>
2026-07-13 02:23:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 const moveButton = document.getElementById("move-random");
 
 function moveRandomEl() {
-    const maxX = window.innerWidth - moveButton.offsetWidth;
-    const maxY = window.innerHeight - moveButton.offsetHeight;
+    const maxX = window.innerWidth - moveButton.offsetWidth - 20;
+    const maxY = window.innerHeight - moveButton.offsetHeight - 20;
 
     moveButton.style.position = "fixed";
     moveButton.style.left = Math.random() * maxX + "px";
     moveButton.style.top = Math.random() * maxY + "px";
 }
 
-document.addEventListener("mousemove", (e) => {
-    const rect = moveButton.getBoundingClientRect();
+moveButton.addEventListener("mouseenter", function (e) {
+    e.preventDefault();
+    moveRandomEl();
+});
 
-    const distance = 120; // pixels
-
-    if (
-        e.clientX > rect.left - distance &&
-        e.clientX < rect.right + distance &&
-        e.clientY > rect.top - distance &&
-        e.clientY < rect.bottom + distance
-    ) {
-        moveRandomEl();
-    }
+moveButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    moveRandomEl();
 });
